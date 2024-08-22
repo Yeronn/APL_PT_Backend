e6f74f43f0b51e1bf8c6ce7ca110025c5985369a
Yeronn <yeronn.f@gmail.com>
2024-08-21 22:43:19 -0500

    Prueba

--- src/main/java/com/aplbackend/pruebaTecnica/controlador/SaludoController.java
+++ src/main/java/com/aplbackend/pruebaTecnica/controlador/SaludoController.java
@@ -2,14 +2,15 @@ package com.aplbackend.pruebaTecnica.controlador;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
 public class SaludoController {
 
     @GetMapping("/saludo/{param}")
-    public String holaMundo(String param) {
-        return "Hola mundo";
+    public String holaMundo(@PathVariable String param) {
+        return "Hola mundo " + param;
     }
     
 }
